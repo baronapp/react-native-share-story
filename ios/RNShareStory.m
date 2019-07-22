c62f1eec9c59e7fe59d652d64a829a2e25877d33
Prescott Rynewicz <prescott@cameo.com>
2019-07-22 16:42:54 -0700

    Updated the share video method.

--- ios/RNShareStory.m
+++ ios/RNShareStory.m
@@ -1,16 +1,12 @@
 
 #import "RNShareStory.h"
 #import <React/RCTLog.h>
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+
 
 @implementation RNShareStory
 
 NSString *const INSTAGRAM_STORIES_SCHEME = @"instagram-stories://share";
-
-- (BOOL)checkInstagramApp
-{
-    NSURL *urlScheme = [NSURL URLWithString:INSTAGRAM_STORIES_SCHEME];
-    return [[UIApplication sharedApplication] canOpenURL:urlScheme];
-}
 
 RCT_EXPORT_MODULE();
 
@@ -18,30 +14,36 @@ RCT_EXPORT_METHOD(shareVideoOnInstagram:(nonnull NSString *)videoUrl
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-    if (![self checkInstagramApp]) {
-        reject(@"ig_share_failure", @"NOT_INSTALLED_ERROR", nil);
-        return;
-    }
-    
-    if (videoUrl == nil) {
-        reject(@"ig_share_failure", @"GENERAL_ERROR", nil );
-    }
-    
-//    NSURL *url = [NSURL URLWithString:@"https://starboard-media.s3.amazonaws.com/v/rv-T8ogXp195.mp4"];
-    NSURL *url = [NSURL URLWithString:videoUrl];
-    NSData *videoData = [NSData dataWithContentsOfURL:(NSURL *)url];
-    
-    // Assign background image asset and attribution link URL to pasteboard
-    NSArray *pasteboardItems = @[@{@"com.instagram.sharedSticker.backgroundVideo" : videoData }];
-    NSDictionary *pasteboardOptions = @{UIPasteboardOptionExpirationDate : [[NSDate date] dateByAddingTimeInterval:60 * 5]};
-    // This call is iOS 10+, can use 'setItems' depending on what versions you support
-    [[UIPasteboard generalPasteboard] setItems:pasteboardItems options:pasteboardOptions];
-    
-    
-    NSURL *urlScheme = [NSURL URLWithString:INSTAGRAM_STORIES_SCHEME];
-    [[UIApplication sharedApplication] openURL:urlScheme options:@{} completionHandler:nil];
-    
-    resolve(@YES);
+
+  NSURL *urlScheme = [NSURL URLWithString:INSTAGRAM_STORIES_SCHEME];
+
+  if (SYSTEM_VERSION_LESS_THAN(@"10.0")) {
+    reject(@"ig_share_failure", @"UNSUPPORTED_IOS_VERSION", nil);
+    return;
+  }
+
+  if ([[UIApplication sharedApplication] canOpenURL:urlScheme]) {
+    reject(@"ig_share_failure", @"NOT_INSTALLED_ERROR", nil);
+    return;
+  }
+
+  if (videoUrl == nil) {
+    reject(@"ig_share_failure", @"GENERAL_ERROR", nil );
+    return;
+  }
+
+  NSURL *url = [NSURL URLWithString:videoUrl];
+  NSData *videoData = [NSData dataWithContentsOfURL:(NSURL *)url];
+
+  // Assign background image asset and attribution link URL to pasteboard
+  NSArray *pasteboardItems = @[@{@"com.instagram.sharedSticker.backgroundVideo" : videoData }];
+  NSDictionary *pasteboardOptions = @{UIPasteboardOptionExpirationDate : [[NSDate date] dateByAddingTimeInterval:60 * 5]};
+  // This call is iOS 10+, can use 'setItems' depending on what versions you support
+  [[UIPasteboard generalPasteboard] setItems:pasteboardItems options:pasteboardOptions];
+
+  [[UIApplication sharedApplication] openURL:urlScheme options:@{} completionHandler:nil];
+
+  resolve(@YES);
 }
 
 @end
