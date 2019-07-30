d617de35d01b824b7605020bcb1080568c652b78
Prescott Rynewicz <prescott@cameo.com>
2019-07-30 15:07:23 -0500

    Cleaned Up package contents.

--- index.ts
+++ index.ts
@@ -3,24 +3,25 @@ import { NativeModules, Platform } from 'react-native';
 const { RNShareStory, RNShareStoryModule } = NativeModules;
 
 
+/* ---- CONSTANTS ---- */
+
 const IG_SHARE_FAIL: string = 'GENERAL_ERROR'
 const NO_INSTALL_ERROR: string = 'NOT_INSTALLED_ERROR'
 const UNSUPPORTED_VERSION: string = 'UNSUPPORTED_IOS_VERSION'
 
-
 /**
  * Method to share a video from a url to instagram.
  * This method currently only works for ios. If called
- * on andriod it will return a rejected Promise.
+ * on android it will return a rejected Promise.
  *
  * @param videoUrl {String} The Video URL to share.
  * @return {Promise<void>} A promise with the action to open instagram.
  */
-const shareVideoOnInstagram = (videoUrl): Promise<void> => {
+const shareVideoOnInstagram = (videoUrl: string): Promise<void> => {
   if (Platform.OS === 'ios') {
     return RNShareStory.shareVideoOnInstagram(videoUrl);
   } else {
-    Promise.reject('Feature not currrently implemented for Android.')
+    return Promise.reject('Feature not currently implemented for Android.')
     // RNShareStoryModule.shareVideoOnInstagram(videoUrl);
   }
 };
